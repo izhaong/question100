daf84719a35392361b51dbc3a3d6fcd05800ca1d
izhaong <izhaong@outlook.com>
2022-05-21 18:05:09 +0800

    feat: 增加webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Date: 2022-05-06 19:12:05
+ * @LastEditTime: 2022-05-21 17:24:12
+ * @LastEditors: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Description: 暂无
+ * @FilePath: /question100/.eslintrc.js
+ */
 module.exports = {
     "env": {
         "browser": true,
---
+++ build/paths.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Date: 2022-05-21 14:34:27
+ * @LastEditTime: 2022-05-21 17:56:52
+ * @LastEditors: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Description: 暂无
+ * @FilePath: /question100/build/paths.js
+ */
+const path = require('path');
+
+
+const srcPath = path.join(__dirname, '', '../src')
+const tplPath = path.join(__dirname, "..", "tpl")
+const distPath = path.join(__dirname, '..', 'dist')
+module.exports = {
+    srcPath,
+    tplPath,
+    distPath
+}
